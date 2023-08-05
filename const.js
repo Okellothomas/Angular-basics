0a7893cd1174ea6aa82b5762909d0a93f51bef4a
Okellothomas <thomasolal33@gmail.com>
2023-08-05 20:28:57 +0300

    add variable substitution

--- const.js
+++ const.js
@@ -1,8 +1,18 @@
 // const variable are block-level scoping for variables
 var thename = 'james';
-var person = {
+"use strict";
+var person = Object.freeze({
     name: '',
     lastname: ''
+});
+var student = {
+    firstname: 'Okello',
+    lastname: 'Thomas'
 };
-person.lastname = 'James';
-console.log(person);
+student.lastname = 'James';
+console.log(student);
+/**
+ * using backstick to write multiple lines
+ */
+var multiple = "Okello thomas \n    is this really your name\n    yes certainly it is my name";
+console.log(multiple);
--- const.ts
+++ const.ts
@@ -19,3 +19,19 @@ console.log(student)
 /**
  * using backstick to write multiple lines
  */
+var multiple = `Okello thomas 
+    is this really your name
+    yes certainly it is my name`
+
+console.log(multiple)
+
+/**
+ * Embeding variabes in our string
+ */
+
+const myschool = 'Kadika primary school!'
+
+let theschool = `Hi okello, my school is ${myschool}, 
+    you will be well pleased to know that`
+
+console.log(myschool)
