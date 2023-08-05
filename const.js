c05e1ba62ded095aac571736e8b37b4c2437aaa7
Okellothomas <thomasolal33@gmail.com>
2023-08-05 23:03:13 +0300

    add

--- const.js
+++ const.js
@@ -22,3 +22,46 @@ console.log(multiple);
 var myschool = 'Kadika primary school!';
 var theschool = "Hi okello, my school is ".concat(myschool, ", \n    you will be well pleased to know that");
 console.log(theschool);
+// The arrow function
+var ping = function () {
+    setTimeout(function () {
+        console.log('ping');
+    }, 1000);
+};
+ping();
+var pong = function () {
+    setTimeout(function () { console.log('Pong'), 1100; });
+};
+pong();
+/**
+ * Object destructuring in javascript
+ */
+var theperson = {
+    first: 'Okello',
+    second: 'Thomas',
+    last: 'Olal'
+};
+var firstname = theperson.first, secondname = theperson.second, lastname = theperson.last;
+console.log(secondname);
+console.log(theperson.second);
+/**
+ * the case of array destructuring
+ */
+var myArray = [1, 3, 4];
+var a = myArray[0], b = myArray[1], c = myArray[2];
+console.log(c);
+console.log(myArray[2]);
+/**
+ * The forloop in typescript
+ */
+var myPerson = {
+    first: "James",
+    last: "Muruki"
+};
+for (var prop in myPerson) {
+    console.log(prop);
+}
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var num in numbers) {
+    console.log(num);
+}
--- const.ts
+++ const.ts
@@ -45,3 +45,57 @@ const ping = () => {
 }
 
 ping()
+
+const pong = () => {
+   setTimeout(()=>{console.log('Pong'),1100})
+}
+
+pong()
+
+/**
+ * Object destructuring in javascript
+ */
+
+const theperson = {
+    first: 'Okello',
+    second: 'Thomas',
+    last: 'Olal'
+}
+
+const { first: firstname, second: secondname, last: lastname } = theperson
+
+console.log(secondname)
+
+console.log(theperson.second)
+
+/**
+ * the case of array destructuring
+ */
+
+const myArray = [1, 3, 4]
+
+const [a, b, c] = myArray
+
+console.log(c)
+
+console.log(myArray[2])
+
+/**
+ * The forloop in typescript
+ */
+
+const myPerson = {
+    first: "James",
+    last: "Muruki"
+}
+
+for (let prop in myPerson) {
+    console.log(prop)
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let num in numbers) {
+    console.log(num)
+}
+
